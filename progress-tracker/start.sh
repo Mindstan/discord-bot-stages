0782debc878a32f9ec33520e01989bb7dde31bad
Gaëtan Blond <gaetan@blond.tf>
2022-11-06 16:42:11 +0100

    Fixed Docker image

--- progress-tracker/start.sh
+++ progress-tracker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sleep 5
-python3 manage.py collectstatic --clear --noinput
-python3 manage.py migrate
-gunicorn -b 0.0.0.0:80 Progression.wsgi:application
+poetry run python manage.py collectstatic --clear --noinput
+poetry run python manage.py migrate
+poetry run gunicorn -b 0.0.0.0:80 Progression.wsgi:application
