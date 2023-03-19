d4627926df1900a9aad8d1a35a79a517271a62bb
Gaëtan Blond <gaetan@blond.tf>
2023-03-19 12:10:35 +0100

    Added simple login route

--- progress-tracker/API/urls.py
+++ progress-tracker/API/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken import views as authtoken_views
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'parcours', views.ParcoursViewSet)
-router.register(r'sujet', views.SujetViewSet)
-router.register(r'candidat', views.CandidatViewSet)
-router.register(r'recherche', views.RechercheViewSet)
-router.register(r'stage', views.StageViewSet)
+router.register(r"parcours", views.ParcoursViewSet)
+router.register(r"sujet", views.SujetViewSet)
+router.register(r"candidat", views.CandidatViewSet)
+router.register(r"recherche", views.RechercheViewSet)
+router.register(r"stage", views.StageViewSet)
 
 urlpatterns = [
-   path('', views.index, name='index'),
-   path('api/', include(router.urls)),
-   path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("", views.index, name="index"),
+    path("login", authtoken_views.obtain_auth_token),
+    path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
